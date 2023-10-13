35b031ef1c8dcc5ba51fa8c421cb4f38caf84326
chta22086 <146318013+chta22086@users.noreply.github.com>
2023-10-13 11:04:15 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
+// Image switcher code
+
 let myImage = document.querySelector('img');
 
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if(mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute ("src","img/firefox2.jpeg");
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/firefox.jpeg') {
+    myImage.setAttribute ('src','images/firefox2.jpeg');
   } else {
-    myImage.setAttribute ("src","img/firefox.jpeg");
+    myImage.setAttribute ('src','images/firefox.jpeg');
   }
-};
+}
 
 // Personalized welcome message code
 
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
 
 function setUserName() {
-  let myName = prompt("あなたの名前を入力してください.");
+  let myName = prompt('Please enter your name.');
   if(!myName) {
     setUserName();
   } else {
-    localStorage.setItem("name", myName);
+    localStorage.setItem('name', myName);
     myHeading.innerHTML = 'Mozilla is cool, ' + myName;
   }
 }
 
-if(!localStorage.getItem("name")) {
+if(!localStorage.getItem('name')) {
   setUserName();
 } else {
-  let storedName = localStorage.getItem("name");
+  let storedName = localStorage.getItem('name');
   myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
 }
 
-myButton.onclick = () => {
+myButton.onclick = function() {
   setUserName();
-};
+}
